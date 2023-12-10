b71523173ef3a5fc1a83cf4cf7c586fd2ff03f03
Himanshu Singh <hsgbxrssm@gmail.com>
2023-12-10 12:46:41 +0530

    first commit

---
+++ app/src/main/java/com/example/iiitn_user/ui/home/HomeFragment.java
@@ -0,0 +1,24 @@
+package com.example.iiitn_user.ui.home;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.iiitn_user.R;
+
+
+public class HomeFragment extends Fragment {
+private TextView iiitnagpur,content,cources,cources1,cources2;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
